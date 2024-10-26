1330b40a820c9170547d3e18cfd8252d2426be48
karimoffabdulatif <akarimoff001@gmail.com>
2024-10-27 01:41:01 +0500

    .

--- app/home-page/page.js
+++ app/home-page/page.js
@@ -4,35 +4,31 @@ import MyPhoto from "../../public/ak_3.jpg";
 import Image from "next/image";
 import Link from "next/link";
 
-
 const Home = () => {
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8">
       <section>
         <div className="flex flex-col lg:flex-row items-center lg:items-start">
-          {/* Mobil o'lchamdagi avatar */}
-          <div className="flex justify-center lg:hidden pt-[30px] sm:pt-[30px]">
+          <div className="flex justify-center lg:hidden pt-[30px] sm:pt-[50px] mt-[45px]"> {/* Mobil o'lchamlar uchun 15px pastga ko'chirish */}
             <Image
               src={MyPhoto}
               alt="Avatar"
-              className="w-[200px] h-[200px] rounded-full"
-              width={200}
-              height={200}
+              className="w-[250px] h-[250px] rounded-full"
+              width={250}
+              height={250}
             />
           </div>
 
-          {/* Matn qismlari */}
-          <div className="text-center lg:text-left mt-4 lg:mt-0 lg:ml-8 md:pt-[150px]">
-          <p className="text-slate-400 text-xl  sm:text-lg lg:text-2xl font-medium">
-  Hello, my name is
-</p>
-
+          <div className="text-center lg:text-left mt-12 lg:mt-0 lg:ml-8 md:pt-[150px]">
+            <p className="text-slate-400 text-xl sm:text-lg lg:text-2xl font-medium">
+              Hello, my name is
+            </p>
 
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 2 }}
-              className="text-white text-[25px]  sm:text-[40px] lg:text-[33px] font-semibold"
+              className="text-white text-[25px] sm:text-[40px] lg:text-[33px] font-semibold"
             >
               Karimov Abdulatif
             </motion.div>
@@ -42,8 +38,8 @@ const Home = () => {
             </h1>
 
             <p className="text-slate-400 lg:w-[700px] font-thin lg:text-[20px] mt-4 px-4 lg:px-0">
-              I am a professional web developer. I can create FrontEnd part of
-              a website. If you interested click the button.
+              I am a professional web developer. I can create FrontEnd part of a
+              website. If you interested click the button.
             </p>
 
             <div className="mt-6">
@@ -55,7 +51,6 @@ const Home = () => {
             </div>
           </div>
 
-          {/* Katta ekranlar uchun avatar */}
           <div className="hidden lg:block lg:pt-[100px] lg:pl-12">
             <Image
               src={MyPhoto}
